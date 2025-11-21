9749cd691108d7e40fc8f0dab2182d857ba0768e
Neider Nieto <nestnidev17@gmail.com>
2025-11-21 01:09:09 -0500

    add experience

---
+++ src/components/Experience.tsx
@@ -0,0 +1,23 @@
+import { experience } from "../lib/experience"
+
+export const Experience = () => {
+    return (
+        <section className="container mx-auto flex flex-col items-center justify-center gap-y-4 my-14">
+            <h2 className="text-5xl font-bold capitalize text-center">¿Por Qué Reservar con Nosotros?</h2>
+            <p className="text-center text-gray-600/70">Te ofrecemos la mejor experiencia para que solo te preocupes por jugar.</p>
+            <div className="flex justify-center gap-4 items-center flex-wrap mt-6">
+                    {experience.map((item, index) => (
+                        <div key={index} className="flex flex-col border border-gray-400/30 justify-between gap-4 items-center bg-white rounded-xl py-4 px-6">
+                            <span className="text-4xl text-btn-dark">
+                                {item.icon}
+                            </span>
+                            <div className="text-center">
+                                <h3 className="text-2xl font-bold">{item.title}</h3>
+                                <p className="text-center text-gray-800/50 text-sm max-w-[335px]">{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+        </section>
+    )
+}
---
+++ src/lib/experience.tsx
@@ -0,0 +1,21 @@
+import { FiClock } from "react-icons/fi";
+import { LuBadgeCheck } from "react-icons/lu";
+import { FiMap } from "react-icons/fi";
+
+export const experience = [
+    {
+        title: "Fácil y rápido",
+        description: "Encuentra y reserva tu cancha ideal en menos de 3 pasos.",
+        icon: <FiClock />
+    },
+    {
+        title: "Confirmación Inmediata",
+        description: "Recibe tu confirmación al instante y asegura tu lugar.",
+        icon: <LuBadgeCheck />
+    },
+    {
+        title: "Variedad de canchas",
+        description: "Explora la más amplia selección de canchas disponibles.",
+        icon: <FiMap />
+    }
+]
