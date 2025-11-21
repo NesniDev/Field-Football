92df5bda29560c137c281bbd245f656ca19f00bc
Neider Nieto <nestnidev17@gmail.com>
2025-11-21 01:08:47 -0500

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="container mx-auto py-4">
+                <p className="text-center text-gray-500/90 text-xs">© {new Date().getFullYear()} Canchas Futbol Chiquinquirá. Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    )
+}
