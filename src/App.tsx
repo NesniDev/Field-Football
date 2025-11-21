beabc93dce66ad1c52e6eab6f0fa3885abe0f551
Neider Nieto <nestnidev17@gmail.com>
2025-11-21 01:08:35 -0500

    Landing Page

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { Experience } from './components/Experience'
+import { Field } from './components/Field'
+import { Hero } from './components/Hero'
+import { Footer } from './components/Footer'
+import { Tournament } from './components/Tournament'
+
+function App() {
+  return (
+    <>
+    <Hero />
+    <Experience />
+    <Field />
+    <Tournament />
+    <Footer />
+    </>
+  )
+}
+
+export default App
