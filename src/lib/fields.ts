6ed66fd3589e3b972d53ff3283e0bd1d77011d72
Neider Nieto <nestnidev17@gmail.com>
2025-11-21 01:09:25 -0500

    add fields football

---
+++ src/lib/fields.ts
@@ -0,0 +1,26 @@
+export const fields = [
+    {
+        image: "images/cancha-1.avif",
+        title: "El Barcelona",
+        address: "Barrio Surinema",
+        punctuation: "5.0",
+    },
+    {
+        image: "images/cancha-2.avif",
+        title: "La Fiera",
+        address: "Barrio la pola",
+        punctuation: "5.0",
+    },
+    {
+        image: "images/cancha-3.avif",
+        title: "Camelot",
+        address: "Centro de eventos Camelot",
+        punctuation: "5.0",
+    },
+    {
+        image: "images/cancha-4.avif",
+        title: "la Futbolera",
+        address: "Avenida Julio Salazar",
+        punctuation: "5.0",
+    }
+]
