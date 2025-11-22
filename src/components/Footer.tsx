5b2b3ba1ae1076ee5c241ddf2892b566cf375819
Neider Nieto <nestnidev17@gmail.com>
2025-11-21 20:55:28 -0500

    footer adjustment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
 export const Footer = () => {
     return (
-        <footer>
-            <div className="container mx-auto py-4">
-                <p className="text-center text-gray-500/90 text-xs">© {new Date().getFullYear()} Canchas Futbol Chiquinquirá. Todos los derechos reservados.</p>
+        <footer className="container px-10 py-4">
+            <div className="flex justify-between items-center gap-4 text-gray-500/90 text-xs">
+                <p className="">© {new Date().getFullYear()} Canchas Futbol Chiquinquirá. Todos los derechos reservados.</p>
+                <div className="flex justify-between items-center gap-4">
+                    <ul className="flex gap-4">
+                        <li><a href="/" className="hover:text-gray-500/70 transition">Soporte</a></li>
+                        <li><a href="/" className="hover:text-gray-500/70 transition">Política de Privacidad</a></li>
+                        <li><a href="/" className="hover:text-gray-500/70 transition">Preguntas Frecuentes</a></li>
+                    </ul>
+                </div>
             </div>
         </footer>
     )
