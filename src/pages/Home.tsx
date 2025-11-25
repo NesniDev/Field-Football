eb2329a321f6e627220adeac0adbc0e103ab79b5
Neider Nieto <nestnidev17@gmail.com>
2025-11-25 09:47:46 -0500

    add browserRouter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
 import { Experience } from "@components/Home/Experience"
 import { Field } from "@components/Home/Field"
 import { Tournament } from "@components/Home/Tournament"
-import { Hero } from "@components/Home/Hero"
-
 export const Home = () => {
   return (
     <>
-      <Hero />
       <Experience />
       <Field />
       <Tournament />
