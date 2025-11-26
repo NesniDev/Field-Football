b927cca970adcb3c614849377d72b5c5952c1e78
Neider Nieto <nestnidev17@gmail.com>
2025-11-26 17:31:26 -0500

    add calendar shadcn

--- src/pages/DetailsField.tsx
+++ src/pages/DetailsField.tsx
@@ -1,12 +1,12 @@
 import { fields } from '@/lib/fields'
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-// import { Calendar } from '@/components/ui/calendar'
+import { Calendar } from '@/components/ui/calendar'
 
 export const DetailsField = () => {
-  // const [date, setDate] = useState(
-  //   new Date(2025, 5, 12)
-  // )
+  const [date, setDate] = useState(
+    new Date(2025, 5, 12)
+  )
   const { slug } = useParams()
     const [activeTab, setActiveTab] = useState('description')
   const info = fields.find(item => item.slug === slug)
@@ -48,14 +48,14 @@ export const DetailsField = () => {
                 <span className='text-sm text-gray-500'>Precio por hora: ${info.price} COP</span>
             </div>
             <div>
-              {/* <Calendar
+              <Calendar
                 required
                 mode="single"
                 selected={date}
                 onSelect={setDate}
                 className="rounded-lg border [--cell-size:--spacing(9)] md:[--cell-size:--spacing(10)] my-5"
                 buttonVariant="ghost"
-              /> */}
+              />
             </div>
             <h2>Horario Disponible</h2>
             <div className='flex flex-wrap justify-between items-center gap-2'>
