3762673cae53bcc816bf475e84ce56f31235b46f
Neider Nieto <nestnidev17@gmail.com>
2025-12-12 17:25:02 -0500

    add values to context

--- src/context/InfoContext.tsx
+++ src/context/InfoContext.tsx
@@ -2,16 +2,28 @@ import { createContext, useState, type ReactNode } from "react";
 
 interface NameContextType {
   name: string;
+  dateSelection: Date;
+  hour: string;
+  address: string;
+  price: string;
   setName: (value: string) => void;
+  setDateSelection: (value: Date) => void;
+  setHour: (value: string) => void;
+  setAddress: (value: string) => void;
+  setPrice: (value: string) => void;
 }
 
 export const NameContext = createContext<NameContextType | null>(null);
 
 export function NameProvider({ children }: { children: ReactNode }) {
   const [name, setName] = useState("");
+  const [dateSelection, setDateSelection] = useState(new Date())
+  const [hour, setHour] = useState("")
+  const [address, setAddress] = useState("")
+  const [price, setPrice] = useState('')
 
   return (
-    <NameContext value={{ name, setName }}>
+    <NameContext value={{ name, setName, dateSelection, setDateSelection, hour, setHour, address, setAddress, price, setPrice }}>
       {children}
     </NameContext>
   );
