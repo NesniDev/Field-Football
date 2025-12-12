572be1117c785536d3094d256b16b9e5fd5169eb
Neider Nieto <nestnidev17@gmail.com>
2025-12-11 20:47:50 -0500

    Add search URL to the football fields and the hero

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,6 +1,19 @@
 import { Header } from "@/components/common/Header";
+import React, {  useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Hero = () => {
+    const [query, setQuery] = useState<string>('')
+    
+    //hook para mover sa otra pagiina
+    const navigate = useNavigate()
+    
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(!query.trim()) return // evita consulats vacias
+        navigate(`/fields?q=${query}`)
+    }
+
     const styles = {
         backgroundImage: 'linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.2)), url(/images/hero.avif)',
         backgroundSize: 'cover',
@@ -16,9 +29,9 @@ export const Hero = () => {
             <div className="flex flex-col justify-center gap-3 container max-w-3xl mx-auto h-full text-center font-orbitron">
                 <h1 className="text-5xl font-bold capitalize" >Tu próximo partido empieza aquí. Encuentra y reserva tu cancha ideal.</h1>
                 <p className="text-sm">Reserva tu cancha favorita.</p>
-                <form className="flex justify-center items-center gap-2 bg-emerald-50/30 rounded-xl px-3 py-2">
-                    <input type="text" className="w-full h-full rounded-lg px-3 py-2 bg-white text-gray-800 placeholder:text-gray-400 " placeholder="El Barcelona..." />
-                    <button type="submit" className="whitespace-nowrap text-sm font-orbitron cursor-pointer hover:bg-btn-dark/90 transition-colors focus:outline-none focus:ring-2 focus:ring-btn-dark text-gray-800 bg-btn-dark px-3 py-2 rounded-lg flex-1">Buscar Cancha</button>
+                <form className="flex justify-center items-center gap-2 bg-emerald-50/30 rounded-xl px-2 py-1.5 focus-within:backdrop-blur-xs transition-all duration-300" onSubmit={handleSubmit}>
+                    <input value={query} onChange={(e) => setQuery(e.target.value)} name="field" type="text" className="w-full h-full outline-none rounded-lg px-3 py-2 bg-white text-gray-800 placeholder:text-gray-400 " placeholder="El Barcelona..." autoComplete="off"/>
+                    <button  type="submit" className="whitespace-nowrap text-sm font-semibold font-orbitron cursor-pointer hover:bg-btn-dark/90 transition-colors focus:outline-none focus:ring-2 focus:ring-btn-dark text-gray-800 bg-btn-dark px-3 py-2 rounded-lg flex-1">Buscar</button>
                 </form>
             </div>
         </section>
--- src/pages/Fields.tsx
+++ src/pages/Fields.tsx
@@ -1,7 +1,8 @@
 import { fields } from "@/lib/fields"
 import { useState } from "react"
-import { NavLink} from "react-router-dom"
+import { NavLink, useLocation, useNavigate} from "react-router-dom"
 import { TbSoccerField  } from "react-icons/tb";
+import { IoMdCloseCircle   } from "react-icons/io";
 
 interface FieldItem {
     image: string;
@@ -23,18 +24,37 @@ interface FieldItem {
 
 export const Fields = () => {
     const [query, setQuery] = useState<string>('')
+    const navigate = useNavigate()
+    const location = useLocation()
+    const params = new URLSearchParams(location.search)
+
+    const queryParams = params.get('q') || ""
     
     const results:FieldItem[] = fields.filter((item: FieldItem) =>
-    item.title.toLowerCase().includes(query.toLowerCase())
+    item.title.toLowerCase().includes(queryParams.toLowerCase())
     );
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault()
+            if(!query.trim()) return // evita consulats vacias
+            navigate(`/fields?q=${query}`)
+        }
     
+        const clean = () => {
+            setQuery('')
+            navigate('/fields')
+        }
     
     return (
         <>
             <div className="max-w-5xl container mx-auto">
-                <form className="flex justify-center items-center max-w-lg mx-auto gap-3 mt-8 bg-gray-400/50  rounded-lg px-4 py-2 group">
-                        <TbSoccerField  className="h-11 w-11 font-normal text-green-900 transition-all duration-300 group-focus-within:rotate-45 group-focus-within:text-green-700"/>
-                        <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" className="w-full h-12 font-orbitron outline-none placeholder:text-gray-500 group-focus:bg-red-600" placeholder="Nombre de la cancha" />
+                <form className="relative flex justify-center font-orbitron items-center transition-all duration-300 max-w-lg mx-auto gap-3 mt-8 bg-gray-400/40  rounded-lg px-4 py-2 group focus-within:ring-2 focus-within:ring-offset-green-200 focus-within:ring-offset-2 focus-within:ring-green-600" onSubmit={handleSubmit}> 
+                        <TbSoccerField  className="h-11 w-11 text-green-900 transition-all duration-300 group-focus-within:rotate-45 group-focus-within:text-green-700"/>
+                        <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" className="w-full h-10 outline-none placeholder:text-gray-500 " placeholder="Nombre de la cancha" />
+                        <button  className="bg-btn-dark px-3 py-2 rounded-lg cursor-pointer hover:bg-btn-dark/90 transition text-sm font-medium">Buscar</button>
+                        {
+                            query && ( <span onClick={() => clean()}  className="absolute -top-2.5 -right-2.5 text-2xl cursor-pointer transition font-bold bg-white rounded-full"><IoMdCloseCircle /></span>)
+                        }
                 </form>
                 <section className="flex flex-wrap items-center justify-center gap-4 my-8 font-orbitron">
                     {
