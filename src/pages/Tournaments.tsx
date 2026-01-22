70461f52fa1af62f4bf3f75d9b8753ea2c900b83
Neider Nieto <nestnidev17@gmail.com>
2026-01-22 13:15:21 -0500

    fix: functionality update

--- src/pages/Tournaments.tsx
+++ src/pages/Tournaments.tsx
@@ -3,15 +3,28 @@ import { FaRegCalendarAlt } from "react-icons/fa";
 import { MdLocationPin } from "react-icons/md";
 import { GiTrophy } from "react-icons/gi";
 import { useState } from 'react';
+import type { Tournament } from '@/models/tournament'
 
 export const Tournaments = () => {
-  const [register, setRegister] = useState('')
+  const [register, setRegister] = useState<string[]>([])
+  const MAX = 3
   
+  const handleRegister = (title: string) => {
+    setRegister(prev => {
+      if (prev.includes(title)) return prev
+      if (prev.length >= MAX) {
+        alert('Ya has inscrito el máximo de torneos')
+        return prev
+      }
+      return [...prev, title]
+    })
+}
+
   return (
     <section className="max-w-5xl mx-auto mt-5">
       <h1 className="my-5 text-3xl font-bold">Explora Nuestros Torneos</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center mb-10 gap-5">
-        {tournament.map((tournament, index) => (
+        {tournament.map((tournament: Tournament, index: number) => (
           <div
             key={index}
             className="flex flex-col items-start bg-white rounded-xl overflow-hidden shadow-md shadow-emerald-200 transition w-62 h-full"
@@ -68,8 +81,8 @@ export const Tournaments = () => {
               
             </div>
             <div className="flex flex-col px-3 py-2 mx-auto">
-              <button onClick={() => setRegister(tournament.title)} disabled={register === tournament.title || tournament.availability === 'Finalizado'} className={` ${register === tournament.title || tournament.availability === 'Finalizado' ? 'cursor-not-allowed bg-gray-400/80 text-gray-200 select-none' : 'cursor-pointer bg-btn-dark hover:bg-btn-dark/90'} block mx-auto  transition-colors focus:outline-none focus:ring-2 focus:ring-btn-dark px-3 py-2 text-xs rounded-lg`}>
-                {tournament.availability === 'Finalizado'  ? 'Finalizado' : register === tournament.title ? 'Inscrito' : 'Inscribirse'}
+              <button onClick={() => handleRegister(tournament.title)} disabled={register.includes(tournament.title) || tournament.availability === 'Finalizado' } className={` ${register.includes(tournament.title) || tournament.availability === 'Finalizado' ? 'cursor-not-allowed bg-gray-400/80 text-gray-200 select-none' : 'cursor-pointer bg-btn-dark hover:bg-btn-dark/90'} block mx-auto  transition-colors focus:outline-none focus:ring-2 focus:ring-btn-dark px-3 py-2 text-xs rounded-lg`}>
+                {tournament.availability === 'Finalizado'  ? 'Finalizado' : register.includes(tournament.title) ? 'Inscrito' : 'Inscribirse'}
               </button>
             </div>
           </div>
