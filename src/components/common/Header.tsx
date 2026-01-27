47ad5f84ac0fc2c238d7b300413af1047b313fc8
Neider Nieto <nestnidev17@gmail.com>
2026-01-27 12:04:18 -0500

    refactor: add isactive functionality

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,6 +6,9 @@ interface HeaderProps {
 
 export const Header = ({color}: HeaderProps) => {
   const {pathname} = useLocation()
+
+  const isActive = (path: string) => pathname === path
+
   return (
     <header className={`max-w-5xl mx-auto`}>
       <nav className="flex justify-between items-center font-orbitron">
@@ -21,22 +24,22 @@ export const Header = ({color}: HeaderProps) => {
             </li>
           )}
           <li>
-            <NavLink to="/fields" className={`transition ${color === 'change' ? 'hover:text-gray-700/80' : 'text-white hover:text-gray-300/80'}`}>
+            <NavLink to="/fields" className={`px-2 py-1 transition duration-300 ${color === 'change' ? ` ${isActive('/fields') ? 'bg-gray-200/15 border border-gray-300 rounded-sm' : 'hover:text-gray-700/80'} ` : 'text-white hover:text-gray-300/80'}`}>
               Canchas
             </NavLink>
           </li>
           <li>
-            <NavLink to="/reservations" className={`transition ${color === 'change' ? 'hover:text-gray-700/80' : 'text-white hover:text-gray-300/80'}`}>
+            <NavLink to="/reservations" className={`px-2 py-1 transition duration-300 ${color === 'change' ? ` ${isActive('/reservations') ? 'bg-gray-200/15 border border-gray-300 rounded-sm' : 'hover:text-gray-700/80'} ` : 'text-white hover:text-gray-300/80'}`}>
               Reservas
             </NavLink>
           </li>
           <li>
-            <NavLink to="/tournaments" className={`transition ${color === 'change' ? 'hover:text-gray-700/80' : 'text-white hover:text-gray-300/80'}`}>
+            <NavLink to="/tournaments" className={`px-2 py-1 transition duration-300 ${color === 'change' ? ` ${isActive('/tournaments') ? 'bg-gray-200/15 border border-gray-300 rounded-sm' : 'hover:text-gray-700/80'} ` : 'text-white hover:text-gray-300/80'}`}>
               Torneos
             </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" className={`transition ${color === 'change' ? 'hover:text-gray-700/80' : 'text-white hover:text-gray-300/80'}`}>
+            <NavLink to="/contact" className={`px-2 py-1 transition duration-300 ${color === 'change' ? ` ${isActive('/contact') ? 'bg-gray-200/15 border border-gray-300 rounded-sm' : 'hover:text-gray-700/80'} ` : 'text-white hover:text-gray-300/80'}`}>
               Contacto
             </NavLink>
           </li>
