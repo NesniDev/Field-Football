6f67604105878c10752e46bc056f68de8637dd6c
Neider Nieto <nestnidev17@gmail.com>
2026-01-29 00:53:15 -0500

    fix: validate search

--- backend/models/field.js
+++ backend/models/field.js
@@ -1,12 +1,14 @@
 import fields from '../fields.json' with { type: 'json' }
 
 export class FieldModel {
-  static async getAll({ text = '', limit, offset }) {
+  static async getAll({ search = '', limit, offset }) {
     let filteredFields = fields
 
-    if (text) {
-      filteredFields = filteredFields.filter(
-        (field) => field.title.toLowerCase() === text.toLowerCase()
+    if (search) {
+      const normalizedSearch = search.toLowerCase()
+
+      filteredFields = filteredFields.filter((field) =>
+        field.title.toLowerCase().includes(normalizedSearch)
       )
     }
 
